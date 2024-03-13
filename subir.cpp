32080d4ef24b70ffc0506a4afc42bd38b09d9f11
juanpahdezreyes <Juanpahdez.r@MacBook-Air-de-Tecnologia.local>
2024-03-13 11:44:47 -0600

    comentario

---
+++ subir.cpp
@@ -0,0 +1,13 @@
+SUBIR
+guardar- command s
+file - save
+git status
+git add ea2.cpp
+git status
+git commit -m "comentario"
+git push
+
+FORK
+FORK
+SSH
+git add git@github.com-juanpahdezreyes:juanpahdezreyes/EA2.git
